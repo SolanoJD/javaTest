c7d43d65c18f78da64d421353f6ea0829c6e676c
SolanoJD <157554351+SolanoJD@users.noreply.github.com>
2024-02-07 20:48:07 -0800

    Update CoinFlip.java

--- CoinFlip.java
+++ CoinFlip.java
@@ -1,1 +1,16 @@
-
+//this made by jd solano tested by me on blueJ
+import java.util.Random;
+public class CoinFlip{
+  public static void main(String[] args){
+    Random r = new Random();
+    int heads =0;
+    int tails =1;
+    r.nextInt(2);
+    if(r.nextInt(2)==0){
+      System.out.println("Heads");
+    }
+    else{
+      System.out.println("Tails");
+    }
+  }
+}
